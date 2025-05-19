0b9b7a678c38fb563148439971abdfa730f4f9d3
Cherry-16 <145067279+Cherry-16@users.noreply.github.com>
2025-05-19 15:26:43 -0600

    solejerciciob

--- Practica_1/Ejercicios.js
+++ Practica_1/Ejercicios.js
@@ -6,7 +6,20 @@
 // nombre = "Ana María"; 
 // var saludo = "Hola, " + nombre + ". Tienes" + edad + " años";
 
-const nombre = "Armando";
-let edad = 25;
-const saludo = `Hola, ${nombre}. Tienes ${edad} años`;
-console.log(saludo); 
+// const nombre = "Armando";
+// let edad = 25;
+// const saludo = `Hola, ${nombre}. Tienes ${edad} años`;
+// console.log(saludo);  
+
+// b. Convierte esta función tradicional a una arrow function que haga exactamente lo
+// mismo: 
+// function cuadrado(numero){
+//     return numero * numero;
+// }
+// console.log(); 
+//muestra en consola en probando con 3 números diferentes 
+
+const cuadrado = (numero) => numero * numero; 
+console.log(cuadrado(3)); 
+console.log(cuadrado(5));
+console.log(cuadrado(2)); 
