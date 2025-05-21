7ae0f7e0eb89bf005d48e29aede64edf34f5f044
Cherry-16 <145067279+Cherry-16@users.noreply.github.com>
2025-05-20 20:08:05 -0600

    solejercicioa

---
+++ Practica_2/Ejercicios.js
@@ -0,0 +1,27 @@
+/*a.
+1. Tienes el siguiente objeto persona.
+2. Extrae los valores de nombre, edad y ciudad usando destructuración.
+3. Luego, muestra un mensaje como: "Me llamo Ivan Isay, tengo 37 años y vivo en Qro." 
+const persona={
+nombre: "Ivan Isay", 
+edad: 37, 
+direccion: {
+ciudad: "Qro",
+pais: MX
+}
+};
+
+Aplica desestructuración aquí 
+Imprime el mensaje
+*/
+
+const persona = {
+    nombre: "Ivan Isay",
+    edad: 37,
+    direccion: {
+        ciudad: "Qro",
+        pais: "MX"
+    }
+    };
+const { nombre, edad, direccion: { ciudad } } = persona; 
+console.log(`Me llamo ${nombre}, tengo ${edad} años y vivo en ${ciudad}.`);
