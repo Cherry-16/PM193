4d1561f7e593a35f86314e5a54fcd4b519858bfa
Cherry-16 <145067279+Cherry-16@users.noreply.github.com>
2025-05-20 20:19:26 -0600

    solejerciciob

--- Practica_2/Ejercicios.js
+++ Practica_2/Ejercicios.js
@@ -12,16 +12,43 @@ pais: MX
 };
 
 Aplica desestructuración aquí 
-Imprime el mensaje
-*/
+Imprime el mensaje*/
 
-const persona = {
-    nombre: "Ivan Isay",
-    edad: 37,
-    direccion: {
-        ciudad: "Qro",
-        pais: "MX"
-    }
-    };
-const { nombre, edad, direccion: { ciudad } } = persona; 
-console.log(`Me llamo ${nombre}, tengo ${edad} años y vivo en ${ciudad}.`);
+// const persona = {
+//     nombre: "Ivan Isay",
+//     edad: 37,
+//     direccion: {
+//         ciudad: "Qro",
+//         pais: "MX"
+//     }
+//     };
+// const { nombre, edad, direccion: { ciudad } } = persona; 
+// console.log(`Me llamo ${nombre}, tengo ${edad} años y vivo en ${ciudad}.`); 
+
+/*b.
+Con el siguiente arreglo de productos, realiza lo siguiente:
+1. Filtra los productos cuyo precio sea mayor a 1000.
+2. Usa .map() para convertir el resultado en un nuevo arreglo con solo los nombres de
+esos productos.
+const productos=[
+{nombre: "Laptop", precio: 12000}
+{nombre: "Mouse", precio: 250}
+{nombre: "Teclado", precio: 750}
+{nombre: "Monitor", precio: 3000}
+];
+
+Tú código aquí
+
+console.log(nombres); [Laptop, Monitor]
+*/ 
+
+const productos = [
+    { nombre: "Laptop", precio: 12000 },
+    { nombre: "Mouse", precio: 250 },
+    { nombre: "Teclado", precio: 750 },
+    { nombre: "Monitor", precio: 3000 }
+];
+const nombres = productos
+    .filter(producto => producto.precio > 1000)
+    .map(producto => producto.nombre);
+console.log(nombres); 
