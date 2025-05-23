1689fb7fc5085c59ff8880cbbeb9124b50b1fd26
Cherry-16 <145067279+Cherry-16@users.noreply.github.com>
2025-05-23 11:39:28 -0600

    solejercicioa

---
+++ Practica_3/Ejercicioa/main.js
@@ -0,0 +1,6 @@
+const restar = require('./utils');
+
+console.log(restar(10, 5));   
+console.log(restar(20, 30));  
+console.log(restar(0, 0));    
+console.log(restar(100, 50)); 
---
+++ Practica_3/Ejerciciob.js
@@ -0,0 +1,5 @@
+/* b.
+1.Crea una función verificarUsuario(usuario) que retorne una promesa.
+2. Si el nombre de usuario es "admin", la promesa se resuelve con "Acceso concedido", si no,
+se rechaza con "Acceso denegado". 
+*/
