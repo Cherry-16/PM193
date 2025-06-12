e01fccf725a5a72b680935a6ea94d170e25bf8d3
Cherry-16 <145067279+Cherry-16@users.noreply.github.com>
2025-06-12 16:14:05 -0600

    children_app.js

--- my-blank/App.js
+++ my-blank/App.js
@@ -3,18 +3,18 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button } from 'react-native';
 
 const Texto=(props)=>{
-  const {contenido}=props
+  const {children}=props
   return(
-    <Text>{contenido}</Text>
+    <Text>{children}</Text>
   )
 }
 /* Zona 2: Main */ 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Texto contenido='Hola'></Texto>
-      <Texto contenido='Mundo'></Texto>
-      <Texto contenido='React Native'></Texto>
+      <Texto>Hola</Texto>
+      <Texto>Mundo</Texto>
+      <Texto>React Native</Texto>
       <Button title='Presionar'></Button>
       <StatusBar style="auto" />
     </View>
