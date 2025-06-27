4d3d2fb820ae6ad41a725f2b13e76035cd25efe1
Cherry-16 <145067279+Cherry-16@users.noreply.github.com>
2025-06-26 21:29:02 -0600

    Repaso

---
+++ proyectoisay/src/screens/SplashScreens.js
@@ -0,0 +1,33 @@
+// src/screens/SplashScreen.js
+import React from 'react';
+import { View, Text, ImageBackground, StyleSheet } from 'react-native';
+
+const SplashScreen = () => {
+  return (
+    <ImageBackground
+      source={require('../assets/fondo_1.jpeg')}
+      style={styles.container}
+      resizeMode="cover"
+    >
+      <Text style={styles.title}>Bienvenido a la App</Text>
+    </ImageBackground>
+  );
+};
+
+export default SplashScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 28,
+    fontWeight: 'bold',
+    color: '#fff',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    padding: 10,
+    borderRadius: 10,
+  },
+});
