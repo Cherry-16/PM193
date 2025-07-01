c4176b524a2c819e3e937b4aa9ca3a68f054f0a5
Cherry-16 <145067279+Cherry-16@users.noreply.github.com>
2025-06-30 19:41:39 -0600

    ScrollView
    Arriba-Abajo

--- my-blank/App.js
+++ my-blank/App.js
@@ -1,23 +1,64 @@
-import React, {useEffect, useState} from 'react'; 
-import {View, StatusBar} from 'react-native';
-import SplashScreen from './src/screens/SplashScreens';
-import HomeScreen from './src/screens/HomeScreens';
+import {
+  ScrollView, // Para hacer el contenido desplazable
+  StatusBar, // Para acceder a la altura de la barra de estado
+  StyleSheet, // Para crear estilos
+  Text
+} from 'react-native';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 
-export default function App() {
-  const [isLoading, setIsLoading] = useState(true);
+const App = () => (
+  <SafeAreaProvider>
+    <SafeAreaView style={styles.container} edges={['top']}>
+      <ScrollView style={styles.scrollView}>
+        <Text style={styles.text}>
+          Este es el texto que ocupara todo el espacio 
+          para que podamos ver el espacio que ocupe el scrollView.
+          Este es el texto que ocupara todo el espacio 
+          para que podamos ver el espacio que ocupe el scrollView.
+          Este es el texto que ocupara todo el espacio 
+          para que podamos ver el espacio que ocupe el scrollView.
+          Este es el texto que ocupara todo el espacio 
+          para que podamos ver el espacio que ocupe el scrollView.
+          Este es el texto que ocupara todo el espacio 
+          para que podamos ver el espacio que ocupe el scrollView.
+          Este es el texto que ocupara todo el espacio 
+          para que podamos ver el espacio que ocupe el scrollView.
+          Este es el texto que ocupara todo el espacio 
+          para que podamos ver el espacio que ocupe el scrollView.
+          Este es el texto que ocupara todo el espacio 
+          para que podamos ver el espacio que ocupe el scrollView.
+          Este es el texto que ocupara todo el espacio 
+          para que podamos ver el espacio que ocupe el scrollView.
+          Este es el texto que ocupara todo el espacio 
+          para que podamos ver el espacio que ocupe el scrollView.
+                    Este es el texto que ocupara todo el espacio 
+          para que podamos ver el espacio que ocupe el scrollView.
+          Este es el texto que ocupara todo el espacio 
+          para que podamos ver el espacio que ocupe el scrollView.
+          Este es el texto que ocupara todo el espacio 
+          para que podamos ver el espacio que ocupe el scrollView.
+          Este es el texto que ocupara todo el espacio 
+          para que podamos ver el espacio que ocupe el scrollView.
+          Este es el texto que ocupara todo el espacio 
+          para que podamos ver el espacio que ocupe el scrollView.
+        </Text>
+      </ScrollView>
+    </SafeAreaView>
+  </SafeAreaProvider>
+);
 
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setIsLoading(false);
-    }, 2500); 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,                             
+    paddingTop: StatusBar.currentHeight 
+  },
+  scrollView: {
+    backgroundColor: 'green',            
+  },
+  text: {
+    fontSize: 42,                        
+    padding: 12,                        
+  },
+});
 
-    return () => clearTimeout(timer);
-  }, []);
-
-  return (
-    <View style={{flex: 1}}>
-      <StatusBar hidden/>
-      {isLoading ? <SplashScreen /> : <HomeScreen />}
-    </View>
-  );
-}
+export default App;
